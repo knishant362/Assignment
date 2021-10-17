e6e556595bbb41b659793e7c8f45acd664635a72
Rohit Jakhar <rohitjakhar940@gmail.com>
2021-10-17 13:48:40 +0530

    Make Code Clean. Reduce code size and use lazy for initlize adapter

--- app/src/main/java/com/trendster/oyelabs/ui/homescreen/HomescreenFragment.kt
+++ app/src/main/java/com/trendster/oyelabs/ui/homescreen/HomescreenFragment.kt
@@ -5,7 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import com.trendster.oyelabs.adapters.GroceriesAdapter
@@ -18,25 +19,31 @@ class HomescreenFragment : Fragment() {
 
     private var _binding: FragmentHomescreenBinding? = null
     private val binding get() = _binding!!
-    private lateinit var topOfferAdapter: TopOfferAdapter
-    private lateinit var exclusiveOfferAdapter: OfferAdapter
-    private lateinit var groceriesAdapter: GroceriesAdapter
-    private lateinit var bestSellingAdapter: OfferAdapter
-    private val viewModel: HomeScreenViewModel by activityViewModels()
+
+    private val topOfferAdapter by lazy { TopOfferAdapter() }
+    private val exclusiveOfferAdapter by lazy { OfferAdapter() }
+    private val groceriesAdapter by lazy {
+        GroceriesAdapter {
+            val action = HomescreenFragmentDirections
+                .actionHomescreenFragmentToProductDetailFragment(it)
+            findNavController().navigate(action)
+        }
+    }
+    private val bestSellingAdapter by lazy { OfferAdapter() }
+
+    private val viewModel: HomeScreenViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentHomescreenBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setupTopOfferRecycler()
         setupExclusiveRecycler()
         setupGroceriesRecycler()
@@ -45,40 +52,34 @@ class HomescreenFragment : Fragment() {
         setupObservers()
     }
 
-    private fun setupTopOfferRecycler() {
-        topOfferAdapter = TopOfferAdapter()
-        binding.rvTop.apply {
-            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+    private fun setupTopOfferRecycler() = binding.apply {
+        rvTop.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
             adapter = topOfferAdapter
             val pagerSnapHelper = PagerSnapHelper()
-            pagerSnapHelper.attachToRecyclerView(binding.rvTop)
-            binding.indicator.attachToRecyclerView(binding.rvTop, pagerSnapHelper)
-            topOfferAdapter.registerAdapterDataObserver(binding.indicator.adapterDataObserver)
+            pagerSnapHelper.attachToRecyclerView(rvTop)
+            indicator.attachToRecyclerView(rvTop, pagerSnapHelper)
+            topOfferAdapter.registerAdapterDataObserver(indicator.adapterDataObserver)
         }
     }
 
-    private fun setupExclusiveRecycler() {
-        exclusiveOfferAdapter = OfferAdapter()
-        binding.rvExclusiveOffer.apply {
-            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-            adapter = exclusiveOfferAdapter
-        }
+    private fun setupExclusiveRecycler() = binding.rvExclusiveOffer.apply {
+        layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        adapter = exclusiveOfferAdapter
     }
 
-    private fun setupGroceriesRecycler() {
-        groceriesAdapter = GroceriesAdapter()
-        binding.rvGroceries.apply {
-            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-            adapter = groceriesAdapter
-        }
+    private fun setupGroceriesRecycler() = binding.rvGroceries.apply {
+        layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        adapter = groceriesAdapter
     }
 
-    private fun setupBestSellingRecycler() {
-        bestSellingAdapter = OfferAdapter()
-        binding.rvBestSelling.apply {
-            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-            adapter = bestSellingAdapter
-        }
+    private fun setupBestSellingRecycler() = binding.rvBestSelling.apply {
+        layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        adapter = bestSellingAdapter
     }
 
     private fun setupObservers() {
@@ -97,7 +98,7 @@ class HomescreenFragment : Fragment() {
         viewModel.groceriesResponse.observe(
             viewLifecycleOwner,
             {
-                groceriesAdapter.setData(it)
+                groceriesAdapter.submitList(it)
             }
         )
         viewModel.bestSellingResponse.observe(
